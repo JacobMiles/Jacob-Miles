df0235a4d755c8f451de633ca332b45f2469bcdd
JacobMiles <36765056+JacobMiles@users.noreply.github.com>
2018-03-19 08:08:18 -0400

    Jacob Miles
    Dictionary program

---
+++ Dictionary program.py
@@ -0,0 +1,12 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #Jacob Miles
+
+dictionary = {
+    "black hole": "a region of space having a gravitational field so intense that no matter or radiation can escape",
+    "asteroid belt": "a region of space between the orbits of Mars and Jupiter where most of the asteroids in our Solar System are found orbiting the Sun.",
+    "galaxy": "a system of millions or billions of stars, together with gas and dust, held together by gravitational attraction.",
+    "meteor": "a small body of matter from outer space that enters the earth's atmosphere, becoming incandescent as a result of friction and appearing as a streak of light.",
+    "nebula": "a cloud of gas and dust in outer space, visible in the night sky either as an indistinct bright patch or as a dark silhouette against other luminous matter.",
+    "supernova": "a star that suddenly increases greatly in brightness because of a catastrophic explosion that ejects most of its mass.",
+    }
