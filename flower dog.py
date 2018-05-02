44fe9d027d6781f753e461dc1237bd69b595c38b
JacobMiles <36765056+JacobMiles@users.noreply.github.com>
2018-05-02 08:08:03 -0400

    Functions

---
+++ flower dog.py
@@ -0,0 +1,24 @@
+
+good_girl = True
+dog = True
+type_of_dog = "Golden Retriever"
+likes_walks = True
+
+def flowers_in_nose(flowers):
+        if good_girl == True and flowers == True:
+                print("She has flowers in her nose, don't judge her it makes her happy.")
+        else:
+                print("She does not currently have flowers in her nose.")
+
+def wag_tail(happy):
+        if happy == True:
+                print("She is wagging her tail because she is happy!")
+        else:
+                print("She is not wagging her tail. :(")
+
+def go_on_walk(wants_walk, good_weather):
+        if good_girl == True and wants_walk == True and good_weather == True and wants_walk == True:
+                print("Let's go on a walk!")
+        else:
+                print("Sorry, we can't go on a walk right now.")
+		
